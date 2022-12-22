8a728ec1ced0b3c2eaf446a14167edef2872377e
Ifenna <101848840+Alaye13@users.noreply.github.com>
2022-12-22 11:29:16 -0500

    Update SCALANOUNIT.scala

--- SCALANOUNIT.scala
+++ SCALANOUNIT.scala
@@ -1,6 +1,4 @@
 // Ifenna Ekwenem
-// CS 310-001
-// Mr Reaser
 object surveytwoappend{
    def append(liztfirst: List[String], liztsecond: List[String]): List[String] = {
        if(liztfirst == Nil){
